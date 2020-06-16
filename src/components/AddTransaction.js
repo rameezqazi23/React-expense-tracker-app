86b2da54ab0b389254e54ad10375d32a1e0a303f
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-06-17 00:08:33 +0500

    deep dive into react components and make partial expense tracker app which is in build

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+export const AddTransaction = () => {
+
+    const [description, setDesciption] = useState();
+    const [transactionAmmount, setTransactionAmmount] = useState();
+    
+
+
+
+    return (
+        <div>
+            <h3>Add New Transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor='description'>
+                        Description
+                    </label>
+                    <input type="text" 
+                           id="description" 
+                           placeholder="Detail of Transaction" 
+                           value={description}
+                           onChange={(e) => setDesciption(e.target.value)}
+                    />
+
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="transactionammount">
+                        Transaction Ammount
+                    </label>
+                    <input type="number" 
+                           id="transactionammount" 
+                           placeholder="Enter transaction ammount" 
+                           value={transactionAmmount}
+                           onChange={(e) => setTransactionAmmount(e.target.value)}
+                    />
+                    
+                </div>
+                <button className="btn"> 
+                    Add Transaction
+                </button>
+            </form>
+        </div>
+    )
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,38 @@
+import React, { createContext, useReducer } from 'react'
+
+//Import Reducer
+import AppReducer from './AppReducer'
+
+//Initialize the state
+
+const initialState = {
+    transactions: [
+        { id: 1, description: "Income 1", transactionAmmount: 1000 }
+        { id: 2, description: "Expense 1", transactionAmmount: -100 }
+        { id: 3, description: "Income 2", transactionAmmount: 2000 }
+        { id: 4, description: "Expense 2", transactionAmmount: -500 }
+
+    ]
+}
+
+//Create Global context
+
+export const GlobalContext = createContext(initialState);
+
+//Create a Provider for the Global Context
+//GlobalContext.Proviedr is actually a wraper
+
+export const GlobalProvider = ({ children }) => {
+
+    cont[state, dispatch] = useReducer(AppReducer, initialState)
+
+    return (
+        <GlobalContext.Provider value={
+            {
+                transactions: state.transactions
+            }
+        }>
+            { children }
+        </GlobalContext.Provider>
+    )
+}
