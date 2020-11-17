efef9e852acabeb39527f0024f4eb6df95ce8dc2
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-11-18 00:33:28 +0500

    deploy on GitHub Actions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -8,13 +8,13 @@ export const Transaction = ({ transaction }) => {
     const { delTransaction } = useContext(GlobalContext)
 
     const sign = transaction.transactionAmmount > 0 ? "+" : "-";
-    const transactionType = transaction.transactionAmmount > 0 ? "plus" : "minus";
+    // const transactionType = transaction.transactionAmmount > 0 ? "plus" : "minus";
 
     return (
 
         <li className="plus">
             {transaction.description}
-
+  
             <span>{sign} ${Math.abs(transaction.transactionAmmount)}</span>
 
             <button onClick={() => delTransaction(transaction.id)} className="delete-btn">X</button>
